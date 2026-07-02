3ef313f8464f37f99285fc8d9edfa3703bafc1b6
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2026-07-02 16:24:41 +0530

    Fix Tailwind breakpoint typo in SubHeading
    Corrected invalid Tailwind CSS breakpoint `mmd:mb-8` to `md:mb-8` in the SubHeading component. Also added image logo and updated favicon.

--- src/design/sub-heading/page.jsx
+++ src/design/sub-heading/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SubHeading(props) {
   return (
-    <div className="flex w-fit gap-3 mb-4 mmd:mb-8 items-center">
+    <div className="flex w-fit gap-3 mb-4 md:mb-8 items-center">
       <div className="relative h-[3px]">
         <div className="w-[35px] h-[2px] bg-rose-700"></div>
         <div className="w-[7px] h-[7px] bg-rose-700 rounded-full absolute inset-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
